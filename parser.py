a6e1bc8ce52385bcec4bcdb51ccad91f3484eafb
Alex Perry <alexpe@seas.upenn.edu>
2016-01-23 17:16:16 -0500

    phrase parser

--- parser.py
+++ parser.py
@@ -1,10 +1,52 @@
 import pattern.en
 from word_checker import WordChecker
+from pyvector import WordVector
 
 class Parser(object):
     def __init__(self):
         self.wordChecker = WordChecker(5000)
+        self.WordVector = WordVector()
 
+    def parseSection(self, section):
+        p = pattern.en.parse(section, chunks = False, lemmata = True).split()
+        finalS = []
+        for sentence in p:
+            s = list(sentence)
+            final = []
+            for count, word in enumerate(sentence):
+                final.append(word[0])
+                if word[1][:3] == 'NNP' or word[1] in ['.',',',':','(',')']:
+                    continue
+                alternates = self.WordVector.get_neighbors(word[2])
+                for test_word in alternates:
+                    if not self.wordChecker.check_word(test_word):
+                        continue
+                    s[count] = test_word
+                    p_test = pattern.en.parse(' '.join(s), chunks = False, lemmata = True).split()
+                    if p_test[0][count][1] == s[0][count][1]:
+                        w = matchType(test_word)
+                        if count == 0:
+                            w = w.capitalize()
+                        final[-1] = w
+                        break
+            finalS.append(' '.join(final))
+        return ''.join(finalS)
+
+    def matchType(self, original, test):
+        oPOS = original[1]
+        nPOS = test[1]
+        word = test[0]
+        if oPOS == 'NN' and nPOS == 'NNS':
+            word = pattern.en.singularize(test[0])
+        if oPOS == 'NNS' and nPOS == 'NN':
+            word = pattern.en.pluralize(test[0])
+        if oPOS == 'JJR' and nPOS == 'JJ':
+            word = pattern.en.comparative(test[0])
+        if oPOS == 'JJS' and nPOS == 'JJ':
+            word = pattern.en.superlative(test[0])
+        if oPOS[:2] == 'VB':
+            word = pattern.en.conjugate(test[0], oPOS)
+        return word
 
     """
     This function return True if the word should be used as is
@@ -22,18 +64,36 @@ class Parser(object):
 
     def getRep(self, words):
         for word in words:
-		p = pattern.en.parse(word, chunks = False, lemmata = True).split()[0][0]
-                if self.POS[0:2] == p[1][0:2]:
-                    #print 'same'
-                    if self.wordChecker.check_word(p[2]):
-                        if self.POS[:2] == 'NN':
-                            if self.POS[3]
-                        return p[2]
+            p = pattern.en.parse(word, chunks = False, lemmata = True).split()[0][0]
+            #print p
+            POS = p[1]
+            #print p, self.POS, POS
+            if self.POS[:2] == p[1][:2]:
+                #print 'same'
+                if self.wordChecker.check_word(p[2]):
+                    word = p[2]
+                    #print word
+                    if self.POS == 'NN' and p[1] == 'NNS':
+                        word = pattern.en.singularize(p[2])
+                    if self.POS == 'NNS' and p[1] == 'NN':
+                        word = pattern.en.pluralize(p[2])
+                    if self.POS == 'JJR' and p[1] == 'JJ':
+                        word = pattern.en.comparative(p[2])
+                    if self.POS == 'JJS' and p[1] == 'JJ':
+                        word = pattern.en.superlative(p[2])
+                    if self.POS[:2] == 'VB':
+                        word = pattern.en.conjugate(p[2], self.POS)
+                    return word
 
 def parseMain():
     p = Parser()
-    p.getLemma('ate')
-    p.getRep(['eat', 'ate', 'food'])
+    print p.parseSection('The quick brown fox jumps over the lazy dog.')
+    #p.getLemma('purring')
+    #print p.POS
+    #print p.getRep(['bless'])
+    #print pattern.en.superlative('easy')
+    #print pattern.en.parse('easiest', lemmata=True)
+    #print pattern.en.parse('a nicer pizza', lemmata=True)
     #print pattern.en.parse('I eat pizza')
 
 if __name__ == "__main__":
