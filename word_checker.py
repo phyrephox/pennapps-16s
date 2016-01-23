9192007a50b7a8370f7aed3bd3e35e133bff4ef0
Alex Perry <alexpe@seas.upenn.edu>
2016-01-22 21:24:44 -0500

    added a common word checker

---
+++ word_checker.py
@@ -0,0 +1,23 @@
+import string
+
+class WordChecker(object):
+    def __init__(self, num_words):
+        self.word_list = []
+        self.word_set = set(self.word_list)
+        f = open('google-10000-english/google-10000-english-usa.txt', 'r')
+        for line in f:
+            self.word_list.append(line[:-1])
+        self.word_set = set(self.word_list[:num_words])
+
+    def check_word(self, word):
+        return string.strip(word) in self.word_set
+
+def wordMain():
+    size = int(raw_input('Enter the number of words to compare against: '))
+    w = WordChecker(size)
+    print (w.word_set)
+    while(True):
+        toCheck = raw_input('Enter a word to check: ')
+        print (w.check_word(toCheck))
+if __name__ == "__main__":
+    wordMain()
