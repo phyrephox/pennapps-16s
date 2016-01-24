a2c77c976239ed7a09c416e06c5c2d33723c65c1
Alex Perry <alexpe@seas.upenn.edu>
2016-01-24 03:09:57 -0500

    working wrapper for cdistance.

--- distance_wrapper.py
+++ distance_wrapper.py
@@ -3,13 +3,28 @@
 # Author : John Hewitt
 # 
 
-import ctypes
+from ctypes import *
+#from numpy.ctypeslib import ndpointer
+clib = cdll.LoadLibrary('./libtest.so')
+clib.get_neighbors.restype = POINTER(c_char_p)
+#clib.get_neighbors.restype = c_char_p
 
 class Distance:
-    testlib = None
 
     def __init__(self):
-        self.testlib = ctypes.CDLL('/home/john/pennapps16s/pennapps-16s/trunk/a.out')
+        #self.testlib = ctypes.CDLL('/home/john/pennapps16s/pennapps-16s/trunk/a.out')
+        #print clib.simple()
+        self.state = clib.construct()
+        #print self.state
 
     def distance(self, word):
-        self.testlib.myprint()
+        #self.testlib.myprint()
+        words = clib.get_neighbors(self.state, word)
+        ws = words[:5]
+        return ws
+        """ws = []
+        for i in range(100):
+            ws[i]"""
+
+d = Distance()
+d.distance('hello')
